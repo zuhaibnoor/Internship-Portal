e052ec7daf05f144b5ea6e7009ae29d88951d16b
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-03-09 23:14:53 +0500

    created the landing page

---
+++ internship-portal/src/App.tsx
@@ -0,0 +1,23 @@
+function App() {  
+
+  return (
+    <>
+      <div style={{
+        backgroundImage: "url(/bg.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        
+      }} className="h-screen w-screen flex justify-center">
+        
+        <form action="" className="mt-20 w-[400px] h-[550px] drop-shadow-lg backdrop-blur-xl rounded-xl">
+          
+        </form>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
