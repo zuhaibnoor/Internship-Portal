4251304f875560679a07f8d546a2c6fdc055d6f5
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-03-10 02:14:15 +0500

    setting up the routes

--- internship-portal/src/pages/Login.tsx
+++ internship-portal/src/pages/Login.tsx
@@ -8,9 +8,24 @@ export default function Login() {
                 backgroundPosition: "center",}} className="h-screen w-screen flex justify-center">
                 
                 <form action="" className="mt-20 w-[400px] h-[550px] drop-shadow-lg backdrop-blur-xl rounded-xl">
-                    <div className="flex flex-row h-[20%] justify-center items-center p-2">
-                        <img src="/logo.png" alt="" className="rounded-[50px] h-[100%]" />
-                        <div className="p-5 text-[35px] font-bold font-sans tracking-wide">Login</div>
+                    <div className="flex flex-col">
+                        
+                        <div className="flex flex-row h-[100px] justify-center items-center p-2">
+                            <img src="/logo.png" alt="" className="rounded-[50px] h-[100%]" />
+                            <div className="p-5 text-[35px] font-bold font-sans tracking-wide">Login</div>
+                        </div>
+
+                        <div className="flex flex-col p-5 ">
+                            <label htmlFor="email" className="text-[16px] text-white">Email</label>
+                            <input type="email" name="email" id="email" className="p-2 border rounded-lg rounded-[90px] bg-transparent" />
+                        </div>
+
+                        <div className="flex flex-col p-5 ">
+                            <label htmlFor="password" className="text-[16px] text-white">Password</label>
+                            <input type="password" name="password" id="password" className="p-2 border rounded-lg rounded-[90px] bg-transparent" />
+                        </div>
+
+
                     </div>
                 </form>
 
--- internship-portal/src/pages/Register.tsx
+++ internship-portal/src/pages/Register.tsx
@@ -9,9 +9,14 @@ export default function Register() {
                 backgroundPosition: "center",}} className="h-screen w-screen flex justify-center">
                 
                 <form action="" className="mt-20 w-[400px] h-[550px] drop-shadow-lg backdrop-blur-xl rounded-xl">
-                    <div className="flex flex-row h-[20%] justify-center items-center p-2">
-                        <img src="/logo.png" alt="" className="rounded-[50px] h-[100%]" />
-                        <div className="p-5 text-[35px] font-bold font-sans tracking-wide">Register</div>
+                    <div className="flex flex-col">
+                        
+                        <div className="flex flex-row h-[100px] justify-center items-center p-2 border-2">
+                            <img src="/logo.png" alt="" className="rounded-[50px] h-[100%]" />
+                            <div className="p-5 text-[35px] font-bold font-sans tracking-wide">Register</div>
+                        </div>
+
+
                     </div>
                 </form>
 
