0f9dc0ad28bfd28cd836d423268ed86d87dbf370
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-03-10 17:39:49 +0500

    login page ready

--- internship-portal/src/pages/Login.tsx
+++ internship-portal/src/pages/Login.tsx
@@ -1,3 +1,6 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+
 export default function Login() {
     return(
         <>
@@ -7,22 +10,33 @@ export default function Login() {
                 backgroundSize: "cover",
                 backgroundPosition: "center",}} className="h-screen w-screen flex justify-center">
                 
-                <form action="" className="mt-20 w-[400px] h-[550px] drop-shadow-lg backdrop-blur-xl rounded-xl">
+                <form action="" className="mt-20 w-[350px] h-[500px] drop-shadow-2xl backdrop-blur-2xl rounded-xl font-sans tracking-wide">
                     <div className="flex flex-col">
                         
                         <div className="flex flex-row h-[100px] justify-center items-center p-2">
-                            <img src="/logo.png" alt="" className="rounded-[50px] h-[100%]" />
-                            <div className="p-5 text-[35px] font-bold font-sans tracking-wide">Login</div>
+                            <img src="/logo.png" alt="" className="rounded-[50px] h-[100%] drop-shadow-2xl" />
+                            <div className="p-5 text-[35px] font-bold font-sans tracking-wide" id = "login">Login</div>
                         </div>
 
                         <div className="flex flex-col p-5 ">
                             <label htmlFor="email" className="text-[16px] text-white">Email</label>
-                            <input type="email" name="email" id="email" className="p-2 border rounded-lg rounded-[90px] bg-transparent" />
+                            <input type="email" name="email" placeholder="abc123@email.com" id="email" className="p-2 border rounded-lg rounded-xl bg-transparent input-focus" />
                         </div>
 
                         <div className="flex flex-col p-5 ">
                             <label htmlFor="password" className="text-[16px] text-white">Password</label>
-                            <input type="password" name="password" id="password" className="p-2 border rounded-lg rounded-[90px] bg-transparent" />
+                            <input type="password" placeholder='Your password' name="password" id="password" className="p-2 border rounded-xl rounded-[90px] bg-transparent input-focus placeholder-opacity-0" />
+                        </div>
+
+
+                        <div className='flex flex-col justify-center items-center mt-5'>
+                            <div className='flex flex-col justify-center items-center  p-2 w-[85%] rounded-2xl bg-white hover:bg-gray-300'>                            
+                                    <Link to="/dashboard" className="h-full w-full text-center">Login</Link>
+                            </div>
+                        </div>
+
+                        <div className='flex flex-col justify-center items-center mt-5'> 
+                            <Link id="getRegistered" to="/Register" className="hover:border hover:rounded-xl p-2 text-white">Want yourself get Registered?</Link>
                         </div>
 
 
