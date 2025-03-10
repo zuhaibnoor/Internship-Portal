e329a206fc6a54910cfe3b63ffb1fd612d2db8ef
zuhaibnoor <zuhaibnoor5@gmail.com>
2025-03-10 19:51:51 +0500

    minor bug fixes

--- internship-portal/src/App.tsx
+++ internship-portal/src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Login/>} />
           <Route path="/register" element={<Register/>} />
-          <Route path="/dashboard/" element={<Dashboard />} />
+          <Route path="/dashboard/" element={<Dashboard/>} />
         </Routes>
       </Router>
 
